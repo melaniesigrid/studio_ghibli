a7967c185d5a8637e13b561bb99482c21232b62d
Melanie Arellano <melaniesigrid@protonmail.com>
2022-03-16 19:00:43 -0600

    Add and style search bar so users can search films

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import FilmCard from './FilmCard';
 import Banner from '../assets/topImage.png';
 
 function HomePage() {
+  const [searchText, setSearchText] = useState('');
   const films = useSelector((state) => state.logic);
+  const filteredFilms = films.filter((item) => Object.keys(item).some((key) => item[key]
+    .toString()
+    .toLowerCase()
+    .includes(searchText.toLocaleLowerCase())));
 
   return (
     <div className="HomePage">
       <div className="BannerDiv">
         <img src={Banner} alt="Ghibli Logo" className="GhibliLogo" />
       </div>
+      <div className="InputContainer">
+        <input value={searchText} onChange={(e) => setSearchText(e.target.value)} type="text" placeholder="Search films..." className="SearchInput" />
+      </div>
       <div className="FilmsContainer">
         {
-          films.map((film) => (
+          filteredFilms.map((film) => (
             <FilmCard
               key={film.filmKey}
               filmKey={film.filmKey}
